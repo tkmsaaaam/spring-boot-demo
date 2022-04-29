e8fa6b323cce34bc5b22bd0a7718aad6779d5584
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-04-29 21:35:18 +0900

    add action && use templates

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,21 @@
 package com.example.springboot;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HelloController {
 
 	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	public String index(Model model) {
+		return "index";
+	}
+
+	@GetMapping("/bonjour")
+	public String bonjour(Model model) {
+		model.addAttribute("message", "Bonjour le monde!");
+		return "bonjour";
 	}
 
 }
