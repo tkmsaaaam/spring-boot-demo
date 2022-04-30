58ff38dd426e16fea481d8ed13ebd280e8d34b46
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-04-30 23:02:51 +0900

    add UserModel

---
+++ src/main/java/com/example/springboot/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboot;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.springboot.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
