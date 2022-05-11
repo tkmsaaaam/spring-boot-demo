1102b499fee45b25a0c71e2952fdd8b4d6bccfd3
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-05-11 22:39:00 +0900

    add WebSecurityConfigTest

---
+++ src/test/java/com/example/springboot/WebSecurityConfigTest.java
@@ -0,0 +1,23 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+// import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WebSecurityConfigTest {
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  public void formLoginWithInvalidCredential() throws Exception {
+    mockMvc.perform(formLogin().user("aaa").password("111")).andExpect(unauthenticated());
+  }
+}
