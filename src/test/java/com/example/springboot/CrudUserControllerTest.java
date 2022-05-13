c7e815b02029d6c9251b20146de1351f6fd9a043
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-05-13 18:34:53 +0900

    add CrudUserControllerTest

---
+++ src/test/java/com/example/springboot/CrudUserControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CrudUserControllerTest {
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  public void getIndex() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/crud-user/index").accept(MediaType.TEXT_HTML))
+				.andExpect(redirectedUrl("http://localhost/login"));
+  }
+}
