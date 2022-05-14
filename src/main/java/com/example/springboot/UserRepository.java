e18c384b972e1a490abde28ba35215446da5dfbb
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-05-14 23:14:04 +0900

    delete unnecessary line

--- src/main/java/com/example/springboot/UserRepository.java
+++ src/main/java/com/example/springboot/UserRepository.java
@@ -3,6 +3,4 @@ package com.example.springboot;
 import com.example.springboot.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
+public interface UserRepository extends CrudRepository<User, Integer> {}
