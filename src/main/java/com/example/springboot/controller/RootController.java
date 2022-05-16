5c5da7e34f12b2535eedd34b872c7760329cbb5b
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-05-16 19:47:17 +0900

    separate controller and mkdir

--- src/main/java/com/example/springboot/controller/RootController.java
+++ src/main/java/com/example/springboot/controller/RootController.java
@@ -1,23 +1,8 @@
 package com.example.springboot.controller;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import com.example.springboot.UserDetailsImpl;
-import com.example.springboot.UserDetailsServiceImpl;
-import com.example.springboot.form.SignupForm;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -27,67 +12,12 @@ public class RootController {
 
 	@GetMapping("/hello")
 	public String index(Model model) {
-		return "hello";
+		return "root/hello";
 	}
 
 	@GetMapping("/bonjour")
 	public String bonjour(Model model) {
 		model.addAttribute("message", "Bonjour le monde!");
-		return "bonjour";
+		return "root/bonjour";
 	}
-
-	@Autowired
-    private UserDetailsServiceImpl userDetailsServiceImpl;
-
-    @GetMapping
-    public String index(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return "index";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
-    @GetMapping("/signup")
-    public String newSignup(SignupForm signupForm) {
-        return "signup";
-    }
-
-    @PostMapping("/signup")
-    public String signup(@Validated SignupForm signupForm, BindingResult result, Model model,
-            HttpServletRequest request) {
-        if (result.hasErrors()) {
-            return "signup";
-        }
-
-        if (userDetailsServiceImpl.isExistUser(signupForm.getUsername())) {
-            model.addAttribute("signupError", "ユーザー名 " + signupForm.getUsername() + "は既に登録されています");
-            return "signup";
-        }
-
-        try {
-            userDetailsServiceImpl.register(signupForm.getUsername(), signupForm.getEmail(), signupForm.getPassword(), "ROLE_USER");
-        } catch (DataAccessException e) {
-            System.out.println(e);
-            model.addAttribute("signupError", "ユーザー登録に失敗しました");
-            return "signup";
-        }
-
-        SecurityContext context = SecurityContextHolder.getContext();
-        Authentication authentication = context.getAuthentication();
-
-        if (authentication instanceof AnonymousAuthenticationToken == false) {
-            SecurityContextHolder.clearContext();
-        }
-
-        try {
-            request.login(signupForm.getUsername(), signupForm.getPassword());
-        } catch (ServletException e) {
-            e.printStackTrace();
-        }
-
-        return "redirect:/";
-    }
-
 }
---
+++ src/main/java/com/example/springboot/controller/SignController.java
@@ -0,0 +1,80 @@
+package com.example.springboot.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import com.example.springboot.UserDetailsImpl;
+import com.example.springboot.UserDetailsServiceImpl;
+import com.example.springboot.form.SignupForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class SignController {
+
+	@Autowired
+  private UserDetailsServiceImpl userDetailsServiceImpl;
+
+  @GetMapping
+  public String index(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    return "sign/index";
+  }
+
+  @GetMapping("/login")
+  public String login() {
+    return "sign/login";
+  }
+
+  @GetMapping("/signup")
+  public String newSignup(SignupForm signupForm) {
+    return "sign/signup";
+  }
+
+  @PostMapping("/signup")
+  public String signup(@Validated SignupForm signupForm, BindingResult result, Model model, HttpServletRequest request) {
+    if (result.hasErrors()) {
+      return "sign/signup";
+    }
+
+    if (userDetailsServiceImpl.isExistUser(signupForm.getUsername())) {
+      model.addAttribute("signupError", "ユーザー名 " + signupForm.getUsername() + "は既に登録されています");
+      return "sign/signup";
+    }
+
+    try {
+      userDetailsServiceImpl.register(signupForm.getUsername(), signupForm.getEmail(), signupForm.getPassword(), "ROLE_USER");
+    } catch (DataAccessException e) {
+      System.out.println(e);
+      model.addAttribute("signupError", "ユーザー登録に失敗しました");
+      return "sign/signup";
+    }
+
+    SecurityContext context = SecurityContextHolder.getContext();
+    Authentication authentication = context.getAuthentication();
+
+    if (authentication instanceof AnonymousAuthenticationToken == false) {
+      SecurityContextHolder.clearContext();
+    }
+
+    try {
+      request.login(signupForm.getUsername(), signupForm.getPassword());
+    } catch (ServletException e) {
+      e.printStackTrace();
+    }
+
+    return "redirect:/";
+  }
+}
