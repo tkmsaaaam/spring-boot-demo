0a979562117b19919fa3697eb29053f388b041ff
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-05-24 21:12:03 +0900

    add postUser

--- src/test/java/com/example/springboot/UsersControllerTest.java
+++ src/test/java/com/example/springboot/UsersControllerTest.java
@@ -25,5 +25,11 @@ public class UsersControllerTest {
       .andExpect(status().is3xxRedirection())
       .andExpect(redirectedUrl("http://localhost/login"));
   }
+
+  @Test
+  public void postUser() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.post("/api/user/add"))
+      .andExpect(status().is4xxClientError());
+  }
   
 }
