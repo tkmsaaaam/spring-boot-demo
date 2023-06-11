de1a40adc27717c80cd91ea772d37ad5b8f39081
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-06-11 11:00:54 +0900

    add src/test/resources/application.properties

--- src/test/java/com/example/springboot/controller/SignControllerTest.java
+++ src/test/java/com/example/springboot/controller/SignControllerTest.java
@@ -5,12 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
